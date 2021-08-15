86991ecec741b55b536bae70e0474bc7aceb73ae
HanByul Yang <hanbyul.yang@gmail.com>
2021-08-15 18:27:37 +0900

    Added course materials     - 3. COmmunicating with Natural Language         - Lesson 3: Speech Recognition             - 5. Quiz: MFCC

---
+++ 3_communicating_with_natural_language/lesson3_speech_recognition/8_quiz_mfcc/function.py
@@ -0,0 +1,16 @@
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+
+
+def wav_to_mfcc(wav_filename, num_cepstrum):
+    """ extract MFCC features from a wav file
+
+    :param wav_filename: filename with .wav format
+    :param num_cepstrum: number of cepstrum to return
+    :return: MFCC features for wav file
+    """
+
+    # TODO implement
+    scipy.io.wavfile.read(wave_fil)
+
+
